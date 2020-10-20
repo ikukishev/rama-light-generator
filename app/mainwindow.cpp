b8df316f79232de5826f6efbc9fd4e4aabdc5c1f
Ivan Kukishev <kukishevivan@gmail.com>
2020-10-20 23:23:17 +0300

    Added experimental widgets

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -2,6 +2,13 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 #include <QUrl>
+#include <QPushButton>
+#include <QLabel>
+#include <QIcon>
+#include <QCheckBox>
+#include <QProgressBar>
+#include <QComboBox>
+#include <QSlider>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -17,10 +24,38 @@ MainWindow::MainWindow(QWidget *parent)
     {
        connect(m_audio_file.get(), SIGNAL(processFinished()), this, SLOT(processFinished()));
        connect(m_audio_file.get(), SIGNAL(positionChanged(const SpectrumData&)), m_spectrograph, SLOT(spectrumChanged( const SpectrumData&)));
-       processFinished();
+       //processFinished();
     }
 
-    m_channelConfigurator->display();
+    //m_channelConfigurator->display();
+    ui->tableWidget->setRowCount(2);
+
+    auto button = new QPushButton("start");
+    button->setIcon(QIcon(":/images/record.png"));
+
+    auto checkBox = new QCheckBox();
+    auto progressBar = new QProgressBar();
+    progressBar->setMaximum(20);
+    progressBar->setMinimum(0);
+    progressBar->setValue(15);
+    auto comboBox = new QComboBox();
+    comboBox->addItem("110");
+    comboBox->addItem("220");
+
+    auto slider = new QSlider();
+    slider->setMaximum(100);
+    slider->setMinimum(0);
+    slider->setOrientation(Qt::Horizontal);
+
+
+
+    ui->tableWidget->setCellWidget(0,0, new QLabel("Path to file"));
+    ui->tableWidget->setCellWidget(0,1, button);
+    ui->tableWidget->setCellWidget(0,2, checkBox);
+    ui->tableWidget->setCellWidget(0,3, progressBar);
+    ui->tableWidget->setCellWidget(0,4, comboBox);
+    ui->tableWidget->setCellWidget(0,5, slider);
+
 }
 
 MainWindow::~MainWindow()
